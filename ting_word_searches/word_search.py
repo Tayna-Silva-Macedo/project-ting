20a71ac44186c9caaddb5665060d440a2e389212
Tayna-Macedo <tayna_sm1996@hotmail.com>
2023-03-07 18:19:47 -0300

    feat: Implementa a função exists_word que retorna uma lista de dicionários com as informações solicitadas

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,23 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    result = []
+
+    for index in range(len(instance)):
+        file = instance.search(index)
+
+        file_result = {
+            "palavra": word,
+            "arquivo": file["nome_do_arquivo"],
+            "ocorrencias": [],
+        }
+
+        for index, line in enumerate(file["linhas_do_arquivo"], 1):
+            if word.lower() in line.lower():
+                file_result["ocorrencias"].append({"linha": index})
+
+        if file_result["ocorrencias"]:
+            result.append(file_result)
+
+    return result
 
 
 def search_by_word(word, instance):
