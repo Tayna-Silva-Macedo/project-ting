723836d07039ebffe0ffa70a0377a7cbed77f20d
Tayna-Macedo <tayna_sm1996@hotmail.com>
2023-03-07 16:10:59 -0300

    feat: Implementação da função process que recebe o caminho do arquivo, uma instancia de fila e slava as informações do arquivo nesta fila

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,30 @@
+import sys
+
+from ting_file_management.file_management import txt_importer
+from ting_file_management.queue import Queue
+
+
+def is_new_file(path_file, instance):
+    for index in range(len(instance)):
+        element = instance.search(index)
+        if element["nome_do_arquivo"] == path_file:
+            return False
+
+    return True
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    if is_new_file(path_file, instance):
+        content = txt_importer(path_file)
+
+        file_info = {
+            "nome_do_arquivo": path_file,
+            "qtd_linhas": len(content),
+            "linhas_do_arquivo": content,
+        }
+
+        instance.enqueue(file_info)
+        return sys.stdout.write(f"{file_info}\n")
 
 
 def remove(instance):
@@ -8,3 +33,6 @@ def remove(instance):
 
 def file_metadata(instance, position):
     """Aqui irá sua implementação"""
+
+
+process("statics/arquivo_teste.txt", Queue())
